83a040388290556eb89f26162e755ad7b25cba11
Logan McDonald <loganmcdona11@gmail.com>
2015-10-27 14:03:25 -0700

    Added the Sinatra gem, added on some template HTML files, functioning simple website is up.

---
+++ Gemfile
@@ -0,0 +1,6 @@
+# A sample Gemfile
+source "https://rubygems.org"
+ruby "2.2.1"
+
+gem "sinatra"
+gem "pry"
--- my-site.rb
+++ my-site.rb
@@ -3,6 +3,24 @@ require "sinatra"
 class MySite < Sinatra::Base
 
   get "/" do
-
+  	erb :index
   end
+
+  get "/index.html" do
+  	erb :index
+  end
+
+
+  get "/about.html" do
+  	erb :about
+  end
+
+  get "/projects.html" do
+  	erb :projects
+  end
+
+  get "/blog.html" do
+  	erb :blog
+  end
+  
 end
